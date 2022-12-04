2939b6514dba80de1c5dde0637152f64fdd7023d
RHsanto <rakibulsanto789@gmail.com>
2022-12-04 19:16:19 +0600

    add

--- src/components/FlightsData/FlightData.tsx
+++ src/components/FlightsData/FlightData.tsx
@@ -2,44 +2,27 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { useGetPokemonByNameQuery } from "../../features/api/apiSlice";
 
-
-
 const FlightData = () => {
-  const {data:flightData}=useGetPokemonByNameQuery("")
-  // console.log(flightData);
+  const { data: flightData } = useGetPokemonByNameQuery("");
+  console.log(flightData);
   return (
     <div className="py-11">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      <div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/400/400/arch" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div><div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/400/400/arch" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div><div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src="https://placeimg.com/400/400/arch" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
-     </div>
-
+        {flightData?.map((data: any) => (
+          <div className="card lg:card-side bg-base-100 shadow-xl">
+            <figure>
+              <img src="https://placeimg.com/400/400/arch" alt="Album" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
