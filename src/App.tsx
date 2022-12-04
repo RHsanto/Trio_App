e52c1940f0b20cff9d0a1a217bea4095d8cf1731
RHsanto <rakibulsanto789@gmail.com>
2022-12-04 20:44:45 +0600

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import { serviceApi } from './features/api/apiSlice';
-import Home from './components/Home';
-import './App.css';
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { serviceApi } from "./features/api/apiSlice";
+import Home from "./components/Home";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Service from "./components/service/Service";
 
 function App() {
   return (
- <Provider store={store}>
-    <ApiProvider api={serviceApi}>
-    <div className="App">
-     <Home/>
-    </div>
-    </ApiProvider>
- </Provider>
+    <Provider store={store}>
+      <ApiProvider api={serviceApi}>
+        <Router>
+          <Routes>
+            <Route  path="/" element={<Home />} />
+            <Route  path="/:id" element={<Service />} />
+          </Routes>
+        </Router>
+      </ApiProvider>
+    </Provider>
   );
 }
 
