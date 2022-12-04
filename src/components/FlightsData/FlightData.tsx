03e67c47b5388f9b1dbb71009b1ea8b63cea551a
RHsanto <rakibulsanto789@gmail.com>
2022-12-04 20:56:40 +0600

    add react router

--- src/components/FlightsData/FlightData.tsx
+++ src/components/FlightsData/FlightData.tsx
@@ -17,7 +17,7 @@ const FlightData = () => {
   
   return (
     <div className="py-11">
-      <h1 className="text-4xl text-center pb-10">Here All Launch Flight Card</h1>
+      <h1 className="text-4xl text-center pb-10">Here All Launch Mission Info Card</h1>
       <div className="grid grid-cols-1 md:grid-cols-4 md:gap-8 gap-6">
         {flightData?.map((data: any) => (
           <div key={data?.mission_name} className="card bg-base-100 shadow-xl">
@@ -25,7 +25,7 @@ const FlightData = () => {
             <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
           </figure>
           <div className="card-body items-center text-center">
-            <h2 className="card-title">{data?.mission_name}</h2>
+            <h2 className="card-title">Mission : {data?.mission_name}</h2>
             <p>{data?.details?.slice(0,100)}</p>
             <div className="flight-info">
               <p>Launch year : {data?.launch_year}</p>
