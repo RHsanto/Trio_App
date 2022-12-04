0bde636f66b653036e464d2af07b730bc4f921af
RHsanto <rakibulsanto789@gmail.com>
2022-12-04 23:45:53 +0600

    add react router

--- src/components/service/Service.tsx
+++ src/components/service/Service.tsx
@@ -10,25 +10,44 @@ const Service = () => {
   return (
     <div className=" container mx-auto mt-7">
       <div className=" flex bg-base-100 shadow-xl border">
-          <div className="card-img w-full border">
-          <img className="w-full" src={singleData?.links?.mission_patch} alt="Album" />
-          </div>
+        <div className="card-img w-full border">
+          <img className="w-full h-96" src={singleData?.links?.mission_patch} alt="Album" />
+        </div>
         <div className="p-6">
-          <h2 className="card-title mb-3">{singleData?.mission_name}</h2>
+          <h2 className="card-title text-3xl mb-3">{singleData?.mission_name}</h2>
           <p className="mb-6">{singleData?.details}</p>
 
-         <p><b>launch date-:</b> {singleData?.launch_date_local}</p>
-         <p><b>launch date unix-:</b> {singleData?.launch_date_unix}</p>
-         <p><b>launch year-:</b> {singleData?.launch_year}</p>
-         <p><b>launch mission-:</b> {singleData?.launch_success === true ? 
-         <span className="text-green-500"> success</span>:
-          <span className="text-red-500">failed</span>}
+          <p>
+            <b>launch date-:</b> {singleData?.launch_date_local}
           </p>
-          <p><b>Mission launch video </b>: <a className="text-blue-700" target="_blank" href={singleData?.links?.video_link}> Youtube</a>
+          <p>
+            <b>launch date unix-:</b> {singleData?.launch_date_unix}
           </p>
-          <p><b>wikipedia  </b>: <a className="text-blue-700" target="_blank" href={singleData?.links?.wikipedia}> wikipedia</a>
+          <p>
+            <b>launch year-:</b> {singleData?.launch_year}
           </p>
-           
+          <p>
+            <b>launch mission-:</b>{" "}
+            {singleData?.launch_success === true ? (
+              <span className="text-green-500"> success</span>
+            ) : (
+              <span className="text-red-500">failed</span>
+            )}
+          </p>
+          <p>
+            <b>Mission launch Info </b>:{" "}
+            <a className="text-blue-700" target="_blank" href={singleData?.links?.video_link}>
+              {" "}
+              Youtube
+            </a>{" "}
+            ||
+            <a className="text-blue-700" target="_blank" href={singleData?.links?.wikipedia}>
+              {" "}
+              wikipedia
+            </a>
+
+            <img className="w-20 mt-4" src={singleData?.links?.mission_patch_small} alt="img" />
+          </p>
         </div>
       </div>
     </div>
